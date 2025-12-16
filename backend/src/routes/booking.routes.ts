5e99653acb90cbff462dfccf85dbd5ab75749f00
Kaarunya Shree Govindarasu <145833192+Kaarunya28@users.noreply.github.com>
2025-12-16 11:22:32 +0530

    Delete backend directory

--- backend/src/routes/booking.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { createBooking } from '../controllers/booking.controller';
-
-const router = Router();
-
-router.post('/', createBooking);
-
-export default router;
